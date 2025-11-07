1df901c70317b89926046323624d35d0bac4c3b8
MahmoudHany <mahmoud.hany.attia@gmail>
2025-11-07 04:33:54 +0200

    some changes

--- Developer.cs
+++ Developer.cs
@@ -29,38 +29,4 @@
 
         }
     }
-
-}
-namespace Inheritance
-{
-    public class Developer : Employee
-    {
-        private const decimal Commission = 0.03m;
-
-        public Developer(string name, int id, decimal loggedHoures, decimal wage , bool taskCompleted) : base(name, id, loggedHoures, wage)
-        {
-            TaskCompleted = taskCompleted;
-        }
-
-        protected bool TaskCompleted { get; set; }
-
-        protected override decimal Calculate()
-        {
-            return base.Calculate() + CalculateBones();
-        }
-
-        private decimal CalculateBones()
-        {
-            return TaskCompleted ? (base.Calculate() * Commission) : 0m;
-        }
-        public override string ToString()
-        {
-            return base.ToString() +
-                $"\nTask Completed : ${(TaskCompleted ? "YES" : "NO")}" +
-                $"\nBones : ${Math.Round(CalculateBones(), 2):N0}" +
-                $"\nNet Salary : ${Math.Round(this.Calculate(), 2):N0}";
-
-        }
-    }
-
 }
---
+++ Maintenance.cs
@@ -0,0 +1,25 @@
+﻿namespace Inheritance
+{
+    public class Maintenance : Employee
+    {
+        private decimal hardship = 1005m;
+
+        public Maintenance(string name, int id, decimal loggedHoures, decimal wage) : base(name, id, loggedHoures, wage)
+        {
+        }
+
+        protected override decimal Calculate()
+        {
+            return base.Calculate() + hardship;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                $"\nHardship : ${Math.Round(hardship, 2):N0}" +
+                $"\nNet Salary : ${Math.Round(this.Calculate(), 2):N0}";
+
+        }
+    }
+
+}
